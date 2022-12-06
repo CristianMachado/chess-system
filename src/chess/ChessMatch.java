d0ce4046cf0aa9889408b1bf86e4f8ab928e48da
Cristian Machado <cris02111996@gmail.com>
2022-12-06 13:48:47 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,7 +21,7 @@ public class ChessMatch {
 	private Board board;
 	private boolean check;
 	private boolean checkMate;
-	private ChessPiece enPassantVulnerable; 
+	private ChessPiece enPassantVulnerable;
 	private ChessPiece promoted;
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
@@ -50,7 +49,7 @@ public class ChessMatch {
 	public boolean getCheckMate() {
 		return checkMate;
 	}
-	
+
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
 	}
@@ -86,9 +85,9 @@ public class ChessMatch {
 			undoMove(source, target, capturePiece);
 			throw new ChessException("You can't put yourself in check");
 		}
-		
+
 		ChessPiece movedPiece = (ChessPiece)board.piece(target);
-				
+
 		//#specialmove promotion
 		promoted = null;
 		if(movedPiece instanceof Pawn) {
@@ -97,7 +96,7 @@ public class ChessMatch {
 				promoted = replacePromotedPiece("Q");
 			}
 		}
-		
+
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
 		if (testCheckMate(opponent(currentPlayer))) {
@@ -105,35 +104,35 @@ public class ChessMatch {
 		} else {
 			nextTurn();
 		}
-			
+
 		//#specialmove en passant
 		if(movedPiece instanceof Pawn && (target.getRow() == source.getRow() - 2 || target.getRow() == source.getRow()+2)) {
 			enPassantVulnerable = movedPiece;
 		}else {
 			enPassantVulnerable = null;
 		}
-		
+
 		return (ChessPiece) capturePiece;
 	}
-	
+
 	public ChessPiece replacePromotedPiece(String type) {
 		if(promoted == null) {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")){
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
-		
+
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
 		piecesOnTheBoard.remove(p);
-		
+
 		ChessPiece newPiece = newPiece(type, promoted.getColor());
 		board.placePiece(newPiece, pos);
 		piecesOnTheBoard.add(newPiece);
-		
-		return newPiece;	
-		
+
+		return newPiece;
+
 	}
 
 	private ChessPiece newPiece(String type, Color color) {
@@ -142,7 +141,7 @@ public class ChessMatch {
 		if(type.equals("Q")) return  new Queen(board, color);
 		return  new Rook(board, color);
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
 		ChessPiece p = (ChessPiece) board.removePiece(source);
 		p.increaseMoveCount();
@@ -169,7 +168,7 @@ public class ChessMatch {
 			board.placePiece(rook, targetT);
 			rook.increaseMoveCount();
 		}
-		
+
 		//#specialmove en passant
 		if(p instanceof Pawn) {
 			if(source.getColumn() != target.getColumn() && capturedPiece == null) {
@@ -184,7 +183,7 @@ public class ChessMatch {
 				piecesOnTheBoard.remove(capturedPiece);
 			}
 		}
-		
+
 		return capturedPiece;
 	}
 
@@ -219,7 +218,7 @@ public class ChessMatch {
 				if(p instanceof Pawn) {
 					if(source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
 						ChessPiece pawn = (ChessPiece)board.removePiece(target);
-						
+
 						Position pawnPosition;
 						if(p.getColor() == Color.WHITE) {
 							pawnPosition = new Position(3, target.getColumn());
